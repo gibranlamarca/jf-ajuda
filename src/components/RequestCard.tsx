eb6cbcd9bfc0457083e31cd9f1d40334e10a1ead
Gibran Millen <gibran.lamarca@engenharia.ufjf.br>
2026-02-26 01:27:28 -0300

    fix: correct pluralization of 'atualização' in RequestCard component

--- src/components/RequestCard.tsx
+++ src/components/RequestCard.tsx
@@ -114,7 +114,7 @@ export default function RequestCard({
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
               d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
           </svg>
-          {r.commentsCount} atualização{r.commentsCount !== 1 ? 'ões' : ''} de ajuda enviada
+          {r.commentsCount} {r.commentsCount !== 1 ? 'atualizações' : 'atualização'} de ajuda enviada
         </div>
       )}
 
@@ -144,7 +144,7 @@ export default function RequestCard({
                     d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                   />
                 </svg>
-                {r.commentsCount} atualização{r.commentsCount !== 1 ? 'ões' : ''}
+                {r.commentsCount} {r.commentsCount !== 1 ? 'atualizações' : 'atualização'}
               </span>
             )}
           </div>
